63fd67472eefc217f7808c32e106bfacf7baa03b
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-08-13 00:24:54 +0530

    :(

--- src/app/api/auth/signup/route.tsx
+++ src/app/api/auth/signup/route.tsx
@@ -1,49 +1,46 @@
-// import bcrypt from "bcrypt";
-
-
 import client from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 import { sign } from "jsonwebtoken";
 import { authentication, random } from "../../../../helpers";
 
-export async function POST(  request: Request){
-    try {
-        const body = await request.json();
-        const {
-            email,
-            password
-          } = body;
-        
-          if(!email || !password){
-            return new NextResponse('Missing info', {status:400})
-          }
+interface RequestBody {
+  email: string;
+  password: string;
+}
 
-          const rand = await random()
+export async function POST(request: Request): Promise<NextResponse> {
+  try {
+    const body: RequestBody = await request.json();
+    const { email, password } = body;
 
-          const hashedPassword = await authentication(rand,password);
-
-          const user = await client.user.create({
-            data: {
-              email,
-              password: hashedPassword,
-              salt : rand
-            }
-          });
-
-          if (typeof process.env.NEXT_PUBLIC_AUTH_SECRET !== 'string') {
-            // Handle the case where secret is undefined
-            throw new Error('Authentication secret is not configured properly.');
-          }
-
-          const token = sign(
-            { id: user.id }, // payload
-            process.env.NEXT_PUBLIC_AUTH_SECRET as string, // secret
-            {} // options
-          );
-
-          return NextResponse.json({user,token});
-        
-    } catch (error:any) {
-        return new NextResponse(error , {status : 400})
+    if (!email || !password) {
+      return new NextResponse('Missing info', { status: 400 });
     }
+
+    const rand = await random();
+    const hashedPassword = await authentication(rand, password);
+
+    const user = await client.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        salt: rand,
+      },
+    });
+
+    if (typeof process.env.NEXT_PUBLIC_AUTH_SECRET !== 'string') {
+      // Handle the case where secret is undefined
+      throw new Error('Authentication secret is not configured properly.');
+    }
+
+    const token = sign(
+      { id: user.id }, // payload
+      process.env.NEXT_PUBLIC_AUTH_SECRET as string, // secret
+      {} // options
+    );
+
+    return NextResponse.json({ user, token });
+  } catch (error: any) {
+    return new NextResponse(error.message, { status: 400 });
+  }
 }
--- src/app/api/flashcard/route.tsx
+++ src/app/api/flashcard/route.tsx
@@ -1,41 +1,39 @@
 import client from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
-export async function POST(  request: Request){
-    try{
-        const body = await request.json();
-        const {
-            question,
-            answer,
-            userId,
-          } = body;
+interface RequestBody {
+  question: string;
+  answer: string;
+  userId: string;
+}
 
-          if(!question || !answer || !userId ){
-            return new NextResponse('Missing info', {status:400})
-          }
+export async function POST(request: Request): Promise<NextResponse> {
+  try {
+    const body: RequestBody = await request.json();
+    const { question, answer, userId } = body;
 
-          const user = await client.user.findUnique({
-            where : { 
-                id : userId
-            }
-          })
-
-
-          if(!user){
-            return new NextResponse('No user Found', {status:404})
-          }
-
-          const flashCard = await client.flashCard.create({
-            data: {
-              question,
-              answer,
-              userId
-            }
-          });
-
-          return NextResponse.json(flashCard);
-
-    }catch (error:any) {
-        return new NextResponse(error , {status : 400})
+    if (!question || !answer || !userId) {
+      return new NextResponse('Missing info', { status: 400 });
     }
+
+    const user = await client.user.findUnique({
+      where: { id: userId }
+    });
+
+    if (!user) {
+      return new NextResponse('No user Found', { status: 404 });
+    }
+
+    const flashCard = await client.flashCard.create({
+      data: {
+        question,
+        answer,
+        userId
+      }
+    });
+
+    return NextResponse.json(flashCard);
+  } catch (error: any) {
+    return new NextResponse(error.message, { status: 400 });
+  }
 }
